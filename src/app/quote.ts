aaf9c66e6e2e98a2d32b623d85b6baae608a377d
uwamahoroange <uwamahoroange002@gmail.com>
2019-08-26 09:52:05 +0200

    Modify form

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -2,8 +2,8 @@ export class Quote {
 showDescription: boolean;
 public upvoteS: number;
 public downvotes: number;
-constructor(public id: number,public Quotess:string, public description:string,public completeDate: Date){
-this.showDescription=false;    
+constructor(public id: number, public Quotess: string, public description: string, public completeDate: Date) {
+this.showDescription = false;
 this.upvoteS = 0;
 this. downvotes = 0;
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -6,53 +6,54 @@ import { Quote } from '../quote';
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
+
 export class QuoteComponent implements OnInit {
   quotes: Quote[] = [
-    new Quote(1,'very next level of your life will demand a different you', 'click on upvote button or downvote button', new Date (2019,8,24)),
+new Quote(1, 'very next level of yourlife will demand a different you', 'click on upvote button or downvote button', new Date(2019, 8, 24)),
   ];
+  preNum: number;
+  lastNum: number;
+  counter: number;
   toggleDetails(index) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
-  preNum: number
-lastNum: number
-counter: number
 
-  upvote(i){
-    this.quotes[i].upvoteS+=1
+  upvote(i) {
+    this.quotes[i].upvoteS += 1;
   }
-  downvote(i){
-    this.quotes[i].downvotes+=1
+  downvote(i) {
+    this.quotes[i].downvotes += 1;
   }
   deleteQuotes(isComplete, index) {
-    let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].Quotess}?`)
+    const toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].Quotess}?`);
 
     if (toDelete) {
-      this.quotes.splice(index,1)
+      this.quotes.splice(index, 1);
     }
   }
 
-addNewQuote (quote){
-  let quotelength = this.quotes.length;
-  quote.id = quotelength+1;
-  quote.completeDate = new Date(quote.completeDate)
-  this.quotes.push(quote)
+  addNewQuote(quote) {
+    const quotelength = this.quotes.length;
+    quote.id = quotelength + 1;
+    quote.completeDate = new Date(quote.completeDate);
+    this.quotes.push(quote);
 
-}
-
-highestUpvote(){
-  this.preNum = 0
-  this.lastNum = 0
-  for (this.counter =0 ; this.counter < this.quotes.length;this.counter++){
-    this.lastNum = this.quotes[ this.counter].upvoteS;
-    if(this.lastNum > this.preNum){
-      this.preNum = this.lastNum}
-    }
-return this.preNum
   }
 
-constructor() { }
+  highestUpvote() {
+    this.preNum = 0;
+    this.lastNum = 0;
+    for (this.counter = 0; this.counter < this.quotes.length; this.counter++) {
+      this.lastNum = this.quotes[this.counter].upvoteS;
+      if (this.lastNum > this.preNum) {
+        this.preNum = this.lastNum;
+      }
+    }
+    return this.preNum;
+  }
 
-ngOnInit() {
-}
+  constructor() { }
 
+  ngOnInit() {
+  }
 }
